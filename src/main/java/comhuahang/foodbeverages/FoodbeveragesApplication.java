b301d4c138b20733b76e0472e37151ea83cd0baa
gaojunli <1757243947@qq.com>
2019-06-10 16:49:56 +0800

    项目初始创建成功

---
+++ src/main/java/comhuahang/foodbeverages/FoodbeveragesApplication.java
@@ -0,0 +1,13 @@
+package comhuahang.foodbeverages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbeveragesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodbeveragesApplication.class, args);
+    }
+
+}
